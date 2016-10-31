e06207cbffc3ef4f90db02d90ada78aabd933716
Tom Vajtay <vajtayt@gmail.com>
2016-10-31 17:28:46 -0400

    Add files via upload
    Alternating, non-random stimulus is now the default setting. Now able to choose a specific amount of trials to run after starting trial.

--- V4.ino
+++ V4.ino
@@ -2,14 +2,14 @@
 // Based off of V3_1 code.
 // Designed to support two DRV2605L boards sharing the 0x5A address, address conflict is resolved through
 // the use of an I2C multiplexer at 0x70.
-// Also assuming MPR121 had address changed to 0x5D. Touch and relese thresholds are 5 and 3 respectively
+// Also assuming MPR121 had address changed to 0x5D. Touch and relese thresholds are 20 and 10 respectively
 // and were edited in the MPR121 header file.
 // Also there is code for pinouts to control an H-bridge to control the solenoid dispensers.
 
 
 #include "Adafruit_MPR121.h"
 #include <Wire.h>	   	//I2C Library
-extern "C" { 
+extern "C" {
 #include "utility/twi.h"  // from Wire library, so we can do bus scanning
 }
 #include "Adafruit_DRV2605.h"  //Haptic Motor library
@@ -20,12 +20,13 @@ Adafruit_MPR121 cap;    //Capacitive Sensor initialization
 
 
 char control_char;
+char selector_char;
 
 bool rew;
 bool pun = true;
 bool first;
 bool autorew = false;
-int count = 0;
+int count = 1;
 int CamPin = 5;		 //Sends TTL signal to camera
 int SolDur = 500;        // Duration for opening Solenoid valve  -- 80msec: 2.5uL
 int SolPin1 = 3;         // First input for H-bridge (Fresh Water)
@@ -34,6 +35,7 @@ int SolPin3 = 8;			// Second input for H-bridge (Salt Water)
 int SolPin4 = 9;
 int irqpin = 12;          // Touch sensor interrupt pin
 int rewPer = 100;        // Period in ms of reward
+int trials;
 long firstlick;
 long trigTime;
 long stimTime;
@@ -42,7 +44,8 @@ int stimDur = 100;
 long elapTime;
 int lickOk = 1500;
 bool trial_start = false;
-
+bool random_toggle = false; //Default is non-random alternating stimulus activation
+bool alternator = true;
 
 
 void setup() {
@@ -63,7 +66,7 @@ void setup() {
 
   // Start I2C
   Wire.begin();
-  
+
   tcaselect(3);  //opens both TCA channels so both DRV boards get activated
 
   //Activates the DRV2605L board(s)
@@ -82,6 +85,7 @@ void setup() {
   Serial.println("Press 6 to stop flush/stim test");
   Serial.println("Press 7 for autoreward, ON(1)/OFF(0)");
   Serial.println("Press 8 for 2 stimulus with punishment(1) OR 1 stimulus no punishment(0)");
+  Serial.println("Press 9 to toggle either random 2 stimulus, or alternating 2 stimulus");
   delay(500);
 }
 
@@ -89,6 +93,12 @@ void loop() {
 
   if (Serial.available()) {
     control_char = Serial.read();
+
+    if (control_char == '9') {
+      random_toggle = !random_toggle;
+      Serial.print("Random:\t");
+      Serial.println(random_toggle);
+    }
 
     if (control_char == '7') {			//Dispenses reward amount of water
       autorew = !autorew;
@@ -121,65 +131,89 @@ void loop() {
     }
 
     if (control_char == '1') {			//Starts the trial
-      count = 0;
+      Serial.println("How many trials do you want in this session?");
+      Serial.println("Press b for 10 trials.");
+      Serial.println("Press n for 30 trials.");
+      Serial.println("Press m for 60 trials.");
+    }
+
+    if (control_char == 'b') {
+      trials = 10;
+      count = 1;
+      Serial.println("Trial Begins");
+      trial_start = true;
+    }
+
+    if (control_char == 'n') {
+      trials = 30;
+      count = 1;
+      Serial.println("Trial Begins");
+      trial_start = true;
+    }
+
+    if (control_char == 'm') {
+      trials = 60;
+      count = 1;
       Serial.println("Trial Begins");
       trial_start = true;
     }
   }
-  if (trial_start) {
-    Serial.println();
-    Serial.println("Trial#\tTime\tEvent");
-  }
-
-  while (trial_start) {
-
-    if (count <= 10) {							// Number of trials per a session
 
 
-      if (Serial.available()) {
-        control_char = Serial.read();
+if (trial_start) {
+  Serial.println();
+  Serial.println("Trial#\tTime\tEvent");
+}
+
+while (trial_start) {
+
+  if (count <= trials) {							// Number of trials per a session
 
 
-        if (control_char == '7') {			//Dispenses reward amount of water if lick is not detected in the reward time period
-          autorew = !autorew;
-          Serial.print("Auto Reward:\t");
-          Serial.print("\t");
-          Serial.println(autorew);
-        }
+    if (Serial.available()) {
+      control_char = Serial.read();
 
-        if (control_char == '8') {
-          pun = !pun;
-          Serial.print("Punishment:\t");
-          Serial.print("\t");
-          Serial.println(pun);
-        }
 
-        if (control_char == '3') {
-          flush_1();
-        }
-
-        if (control_char == '4') {			//Flushes out the salt-water line
-          flush_2();
-        }
-
-        if (control_char == '2') {			//Stops the trial
-          end_session();
-        }
+      if (control_char == '7') {			//Dispenses reward amount of water if lick is not detected in the reward time period
+        autorew = !autorew;
+        Serial.print("Auto Reward:\t");
+        Serial.print("\t");
+        Serial.println(autorew);
       }
 
-      if (pun) {
-        elapTime = 0;
-        first = false;
-        rew = false; 	//For autoreward activation, only turned true when reward lick detected which blocks autoreward
-        trigTime = millis();
-        digitalWrite(CamPin, HIGH);       // Sends TTL to Master  9 or camera controller
-
-        early(2000);			// The function that detects an early lick in first two seconds of trial
-        stimTime = millis();
-        Serial.print(count);
+      if (control_char == '8') {
+        pun = !pun;
+        Serial.print("Punishment:\t");
         Serial.print("\t");
-        Serial.print(stimTime - trigTime);
+        Serial.println(pun);
+      }
 
+      if (control_char == '3') {
+        flush_1();
+      }
+
+      if (control_char == '4') {			//Flushes out the salt-water line
+        flush_2();
+      }
+
+      if (control_char == '2') {			//Stops the trial
+        end_session();
+      }
+    }
+
+    if (pun) {
+      elapTime = 0;
+      first = false;
+      rew = false; 	//For autoreward activation, only turned true when reward lick detected which blocks autoreward
+      trigTime = millis();
+      digitalWrite(CamPin, HIGH);       // Sends TTL to Master  9 or camera controller
+
+      early(2000);			// The function that detects an early lick in first two seconds of trial
+      stimTime = millis();
+      Serial.print(count);
+      Serial.print("\t");
+      Serial.print(stimTime - trigTime);
+      if (random_toggle) {
         int randNum = random(0, 6);
 
         if (randNum <= 3) {
@@ -191,172 +225,185 @@ void loop() {
           stimNum = 2;
           Serial.println("\tStim 2");
         }
+      }
 
-        while (elapTime < lickOk) {     //For 1.5 seconds after stim mouse can lick sensor to recieve reward or punishment.
-          if (elapTime < stimDur) {
-
-            if (stimNum == 1) {
-              //for stim1
-              
-              drv.setWaveform(0, stim_effect1);  // the effect
-              drv.setWaveform(1, 0);   // end waveform
-              tcaselect(2);
-              drv.go();
-            }
-
-            else if (stimNum == 2) {
-              //for stim2
-              
-              drv.setWaveform(0, stim_effect2);  // the effect
-              drv.setWaveform(1, 0);   // end waveform
-              tcaselect(1);
-              drv.go();
-            }
-          }
-
-
-          if (!checkInterrupt()) {
-            //read the touch state from the MPR121
-            Wire.requestFrom(0x5D, 2);
-
-            byte LSB = Wire.read();
-            byte MSB = Wire.read();
-
-            uint16_t touched = ((MSB << 8) | LSB); //16bits that make up the touch states
-
-            if (touched) {
-              if (!first) {
-                firstlick = millis() - trigTime;
-
-                if (stimNum == 1) {				//Only first lick in the 1.5 timeframe will give reward or punishment
-                  Serial.print(count);
-                  Serial.print("\t");
-                  Serial.print(firstlick);
-                  Serial.println("\tReward Lick");
-                  rew = true;
-                  first = true;
-                  reward();
-                }
-
-                else if (stimNum == 2) {
-                  Serial.print(count);
-                  Serial.print("\t");
-                  Serial.print(firstlick);
-                  Serial.println("\tPunishment Lick");
-                  punishment();
-                  first = true;
-                }
-
-              }
-              else if (first & stimNum == 1) {
-                Serial.print(count);
-                Serial.print("\t");
-                Serial.print(millis() - trigTime);
-                Serial.println("\tLick Good");
-              }
-              else if (first & stimNum == 2) {
-                Serial.print(count);
-                Serial.print("\t");
-                Serial.print(millis() - trigTime);
-                Serial.println("\tLick Bad");
-              }
-            }
-          }
-          elapTime = millis() - stimTime; //Updates the time for the while loop
+      else if (!random_toggle) {
+        if (alternator) {
+          stimNum = 1;
+          Serial.println("\tStim 1");
+          alternator = !alternator;
+        }
+        else if (!alternator) {
+          stimNum = 2;
+          Serial.println("\tStim 2");
+          alternator = !alternator;
         }
       }
 
-      if (!pun) {        //If there is no punishment or only one stimulus
-        elapTime = 0;
-        first = false;
-        rew = false; 	//For autoreward activation, only turned true when reward lick detected which blocks autoreward
-        trigTime = millis();
-        digitalWrite(CamPin, HIGH);       // Sends TTL to Master  9
+      while (elapTime < lickOk) {     //For 1.5 seconds after stim mouse can lick sensor to recieve reward or punishment.
+        if (elapTime < stimDur) {
 
-        early(2000);					// The function that detects an early lick in first two seconds of trial
-        stimTime = millis();
-        stimNum = 1;
-        Serial.print(count);
-        Serial.print("\t");
-        Serial.print(stimTime - trigTime);
-        Serial.println("\tStim 1");
+          if (stimNum == 1) {
+            //for stim1
 
-
-        while (elapTime < lickOk) {     //For 1.5 seconds after stim mouse can lick sensor to recieve reward or punishment.
-          if (elapTime < stimDur) {
-
-            if (stimNum == 1) {
-              //for stim1
-              drv.setWaveform(0, stim_effect1);  // the effect
-              drv.setWaveform(1, 0);   // end waveform
-              tcaselect(2);
-              drv.go();
-            }
+            drv.setWaveform(0, stim_effect1);  // the effect
+            drv.setWaveform(1, 0);   // end waveform
+            tcaselect(2);
+            drv.go();
           }
 
+          else if (stimNum == 2) {
+            //for stim2
 
-          if (!checkInterrupt()) {
-            //read the touch state from the MPR121
-            Wire.requestFrom(0x5D, 2);
+            drv.setWaveform(0, stim_effect2);  // the effect
+            drv.setWaveform(1, 0);   // end waveform
+            tcaselect(1);
+            drv.go();
+          }
+        }
 
-            byte LSB = Wire.read();
-            byte MSB = Wire.read();
 
-            uint16_t touched = ((MSB << 8) | LSB); //16bits that make up the touch states
+        if (!checkInterrupt()) {
+          //read the touch state from the MPR121
+          Wire.requestFrom(0x5D, 2);
 
-            if (touched) {
-              if (!first) {
-                firstlick = millis() - trigTime;
+          byte LSB = Wire.read();
+          byte MSB = Wire.read();
 
-                if (stimNum == 1) {										//Only first lick in the 1.5 timeframe will give reaward or punishment
-                  Serial.print(count);
-                  Serial.print("\t");
-                  Serial.print(firstlick);
-                  Serial.println("\tReward Lick");
-                  rew = true;
-                  first = true;
-                  reward();
-                }
-              }
-              else if (first & stimNum == 1) {
+          uint16_t touched = ((MSB << 8) | LSB); //16bits that make up the touch states
+
+          if (touched) {
+            if (!first) {
+              firstlick = millis() - trigTime;
+
+              if (stimNum == 1) {				//Only first lick in the 1.5 timeframe will give reward or punishment
                 Serial.print(count);
                 Serial.print("\t");
-                Serial.print(millis() - trigTime);
-                Serial.println("\tLick Good");
+                Serial.print(firstlick);
+                Serial.println("\tReward Lick");
+                rew = true;
+                first = true;
+                reward();
               }
+
+              else if (stimNum == 2) {
+                Serial.print(count);
+                Serial.print("\t");
+                Serial.print(firstlick);
+                Serial.println("\tPunishment Lick");
+                punishment();
+                first = true;
+              }
+
+            }
+            else if (first & stimNum == 1) {
+              Serial.print(count);
+              Serial.print("\t");
+              Serial.print(millis() - trigTime);
+              Serial.println("\tLick Good");
+            }
+            else if (first & stimNum == 2) {
+              Serial.print(count);
+              Serial.print("\t");
+              Serial.print(millis() - trigTime);
+              Serial.println("\tLick Bad");
             }
           }
-          elapTime = millis() - stimTime; //Updates the time for the while loop
         }
-
-
+        elapTime = millis() - stimTime; //Updates the time for the while loop
       }
+    }
+
+    if (!pun) {        //If there is no punishment or only one stimulus
+      elapTime = 0;
+      first = false;
+      rew = false; 	//For autoreward activation, only turned true when reward lick detected which blocks autoreward
+      trigTime = millis();
+      digitalWrite(CamPin, HIGH);       // Sends TTL to Master  9
+      early(2000);					// The function that detects an early lick in first two seconds of trial
+      stimTime = millis();
+      stimNum = 1;
+      Serial.print(count);
+      Serial.print("\t");
+      Serial.print(stimTime - trigTime);
+      Serial.println("\tStim 1");
 
 
-      if (stimNum == 1) {			// If after 1.5 sec trial mouse did not lick for reward, reward given if autoreward is turned on Auto reward works with or without punishment
-        if (autorew) {
-          if (!rew) {
-            reward();
+      while (elapTime < lickOk) {     //For 1.5 seconds after stim mouse can lick sensor to recieve reward or punishment.
+        if (elapTime < stimDur) {
+
+          if (stimNum == 1) {
+            //for stim1
+            drv.setWaveform(0, stim_effect1);  // the effect
+            drv.setWaveform(1, 0);   // end waveform
+            tcaselect(2);
+            drv.go();
           }
         }
+
+
+        if (!checkInterrupt()) {
+          //read the touch state from the MPR121
+          Wire.requestFrom(0x5D, 2);
+
+          byte LSB = Wire.read();
+          byte MSB = Wire.read();
+
+          uint16_t touched = ((MSB << 8) | LSB); //16bits that make up the touch states
+
+          if (touched) {
+            if (!first) {
+              firstlick = millis() - trigTime;
+
+              if (stimNum == 1) {										//Only first lick in the 1.5 timeframe will give reaward or punishment
+                Serial.print(count);
+                Serial.print("\t");
+                Serial.print(firstlick);
+                Serial.println("\tReward Lick");
+                rew = true;
+                first = true;
+                reward();
+              }
+            }
+            else if (first & stimNum == 1) {
+              Serial.print(count);
+              Serial.print("\t");
+              Serial.print(millis() - trigTime);
+              Serial.println("\tLick Good");
+            }
+          }
+        }
+        elapTime = millis() - stimTime; //Updates the time for the while loop
       }
 
-      late(3500); 			//For 3.5 secs after LickOk period all licks will be recorded as late, no punishment or reward given.
-      Serial.println("Trial End");
-      count = count + 1;
-      digitalWrite(CamPin, LOW);
 
-      delay(4000);   //Inter Trial Interval of 4 secs
     }
 
 
-    else {
-      count = 0;
-      Serial.println("Session Finished");
-      trial_start = false; //Possibly redundant since end_session does a soft restart
-      end_session();
+    if (stimNum == 1) {			// If after 1.5 sec trial mouse did not lick for reward, reward given if autoreward is turned on Auto reward works with or without punishment
+      if (autorew) {
+        if (!rew) {
+          reward();
+        }
+      }
     }
+
+    late(3500); 			//For 3.5 secs after LickOk period all licks will be recorded as late, no punishment or reward given.
+    Serial.println("Trial End");
+    count = count + 1;
+    digitalWrite(CamPin, LOW);
+
+    delay(3000);   //Inter Trial Interval of 4 secs
   }
+
+
+  else {
+    count = 1;
+    Serial.println("Session Finished");
+    trial_start = false; //Possibly redundant since end_session does a soft restart
+    end_session();
+  }
+}
 
 }
 
@@ -494,10 +541,10 @@ void stim_test() {
 
 void tcaselect(uint8_t i) {
   if (i > 3) return;
- 
+
   Wire.beginTransmission(0x70);
   Wire.write(i);
-  Wire.endTransmission();  
+  Wire.endTransmission();
 }
 
 
